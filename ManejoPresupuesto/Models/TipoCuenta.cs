1bd2df6e52fa6df8b4c328970e0dd9f30d437701
JhordanPacheco100 <21890257@ittizimin.edu.mx>
2024-10-07 23:05:32 -0600

    subiendo

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Validaciones;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [PrimeraLetraMayuscula]
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas",
+            AdditionalFields = nameof(Id))]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
